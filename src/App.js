306681d8f4fe01e85eae7bd614e7b436230a8d1c
chandankl5 <chandakl0310@gmail.com>
2021-02-11 08:25:23 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class App extends React.Component {
+ render() {
+  return(
+   <div>
+   <h1> My App Component configured uing babel and webpack.... </h1>
+   </div>
+  );
+ }
+}
+export default App
